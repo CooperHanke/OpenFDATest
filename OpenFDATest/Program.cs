973bd9840c8d9d6267f3c3c76345a602761f2355
Cooper Hanke <cooper.hanke@gmail.com>
2018-07-13 10:02:00 -0500

    Separated methods into partial classes; made method for data access static to try to save memory and make a smaller footprint; moving to phase 2

--- OpenFDATest/Program.cs
+++ OpenFDATest/Program.cs
@@ -2,72 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
 
 namespace OpenFDATest
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            //string url = "https://api.fda.gov/drug/label.json?search=";
-            //string search = "caffeine";
-            var request = (HttpWebRequest)WebRequest.Create("https://api.fda.gov/drug/label.json?search=caffeine&limit=1");
-
-            request.Method = "GET";
-            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
-            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
-
-            var response = (HttpWebResponse)request.GetResponse();
-
-            string content = string.Empty;
-            using (var stream = response.GetResponseStream())
-            {
-                using (var sr = new StreamReader(stream))
-                {
-                    content = sr.ReadToEnd();
-                }
-            }
-            Console.WriteLine("Finished reading the response");
-
-            try
-            {
-                JObject results = JObject.Parse(content);
-                var test = results["meta"];
-                var test2 = results["results"][0]; // grab the first result then
-                //Console.WriteLine($"Type of results: {test["effective_date"].Type}");
-                int count = 0; // count the keys then
-                foreach (JProperty prop in test2)
-                {
-                    Console.WriteLine(prop.Name);
-                    count++;
-                }
-                Console.WriteLine($"Number of keys in our results: {count}"); // find out the number of keys bc we will need those later on
-
-                count = 0;
-                foreach (JProperty prop in test2)
-                {
-                    if (prop.Name.Equals("openfda")) // and now we have proven we can sift by keys
-                    {
-                        Console.WriteLine("Nope, found openfda, count should be one less now");
-                        continue;
-                    }
-                    count++;
-                }
-                Console.WriteLine($"Number of keys in our results: {count} (should be less really)"); // find out the number of keys bc we will need those later on
-                Console.WriteLine($"Type of results[\"meta\"] is {test.Type}"); // meta = object
-                Console.WriteLine($"Type of results[\"results\"] is {test2.Type}"); // results = array
-            }
-            catch (Newtonsoft.Json.JsonReaderException ex)
-            {
-                Console.WriteLine("Failed to get the JSON! Nooooo!");
-                Console.WriteLine($"{ex.Message}");
-            }
-            Console.WriteLine("Press enter to end the program...");
+            GetMedInfo("xanax");
             Console.ReadKey();
         }
     }
---
+++ OpenFDATest/RetrieveMedicineInfo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+
+namespace OpenFDATest
+{
+    /*
+     * This class is meant to separate the logic of getting the medicine data from the rest of the program
+     */
+
+    partial class Program
+    {
+        public static void GetMedInfo(string desiredSearch)
+        {
+            string url = "https://api.fda.gov/drug/label.json?search=";
+            var request = (HttpWebRequest)WebRequest.Create($"{url}{desiredSearch}&limit=1");
+
+            request.Method = "GET";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            string content = string.Empty;
+            using (var stream = response.GetResponseStream())
+            {
+                using (var sr = new StreamReader(stream))
+                {
+                    content = sr.ReadToEnd();
+                }
+            }
+            Console.WriteLine("Finished reading the response");
+            SortedDictionary<string, string> thing = SortedMedInfo(content);
+            if (thing.Count > 0)
+            {
+                Console.WriteLine($"We have {thing.Count} entries from the SortedMedInfo method");
+            }
+        }
+        
+        public static SortedDictionary<string, string> SortedMedInfo (string content)
+        {
+            SortedDictionary<string, string> temp = new SortedDictionary<string, string>();
+            try
+            {
+                JObject results = JObject.Parse(content);
+                var meta = results["meta"];
+                var data = results["results"][0]; // grab the first result then
+                foreach (JProperty prop in data)
+                {
+                    temp.Add(prop.Name, prop.Value.ToString());
+                }
+            }
+            catch (Newtonsoft.Json.JsonReaderException ex)
+            {
+                Console.WriteLine("Failed to get the JSON! Nooooo!");
+                Console.WriteLine($"{ex.Message}");
+            }
+            return temp;
+        }
+    }
+}
